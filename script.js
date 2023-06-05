af765fb5ae8e0080ba01487aa0e25dd191cd1e46
daycarrera07 <daiana_carrera@hotmail.com.ar>
2023-06-05 17:15:11 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function showAlert () { alert("¡Hola, esta es una alerta desde JavaScript!"); 
+}
+
+const form = document.getElementById("myForm");
+
+
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email);
+}
+
+function validateForm () {
+    const input = document.getElementById("email");
+    const email = input.value;
+
+    if (!validateEmail(email)) {
+        alert("Por favor ingrese un correo electronico valido.");
+    } else {
+        alert("Correo electronico enviado correctamente.");
+    }
+}
+if (form) {
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        validateForm();
+    });
+} 
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
+
+
